5b2b1f9685c69fb16ceb3d26bf2bd999dc99b5df
khangdasac <145788969+khangdasac@users.noreply.github.com>
2026-06-19 20:57:41 +0700

    chỉnh sửa

--- src/module/game/game.controller.ts
+++ src/module/game/game.controller.ts
@@ -1,10 +1,8 @@
 import { Body, Controller, Post, UseGuards } from "@nestjs/common";
 import { JwtAuthGuard } from "../auth/guards/jwt-auth.guard";
-import { CreateGameDto } from "./dto/create-game.dto";
 import { GameService } from "./game.service";
 
 @Controller('game')
 export class GameController {
     constructor(private readonly gameService: GameService) { }
-
 }
