0893375607f53da749662312fcaae9552fcd2029
khangdasac <145788969+khangdasac@users.noreply.github.com>
2026-06-20 00:26:19 +0700

    clean code lần 2

--- src/module/game/dto/find-match-req.dto.ts
+++ src/module/game/dto/find-match-req.dto.ts
@@ -1,6 +1,8 @@
+import { Type } from 'class-transformer';
 import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class FindMatchReqDto {
+    @Type(() => Number)
     @IsInt()
     @IsNotEmpty()
     gameModeId: number;
--- src/module/game/dto/make-move-req.dto.ts
+++ src/module/game/dto/make-move-req.dto.ts
@@ -1,6 +1,8 @@
+import { Type } from 'class-transformer';
 import { IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class MakeMoveReqDto {
+    @Type(() => Number)
     @IsInt()
     @IsNotEmpty()
     gameId: number;
